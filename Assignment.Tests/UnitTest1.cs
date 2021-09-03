1361b1a353c934990a3f32de8c17b9884cb73c09
RasmusUK <74377458+RasmusUK@users.noreply.github.com>
2021-09-03 16:35:00 +0200

    Implemented error handling (#4)
    Co-authored-by: rkri <rkri@itu.dk>

--- Assignment.Tests/UnitTest1.cs
+++ Assignment.Tests/UnitTest1.cs
@@ -13,7 +13,11 @@ namespace Assignment.Tests
             var divisibleByFour = Program.IsLeapYear(2004);
             var divisibleByFourAndHundred = Program.IsLeapYear(1600);
             var divisibleByFourAndNotHundred = Program.IsLeapYear(1700);
+            var yearOnLimit = Program.IsLeapYear(1581);
+            var yearBeforeLimit = Program.IsLeapYear(800);
 
+            Assert.False(yearOnLimit);
+            Assert.False(yearBeforeLimit);
             Assert.False(notDivisibleByFour);
             Assert.True(divisibleByFour);
             Assert.True(divisibleByFourAndHundred);
@@ -34,6 +38,14 @@ namespace Assignment.Tests
             var actual = Program.GetLeapYearResponse("2001");
 
             Assert.Equal("nay", actual);
+        }
+
+        [Fact]
+        public void UserInputNotInteger()
+        {
+            var actual = Program.GetLeapYearResponse("20ab");
+
+            Assert.Equal("Please input a valid input", actual);
         }
     }
 }
