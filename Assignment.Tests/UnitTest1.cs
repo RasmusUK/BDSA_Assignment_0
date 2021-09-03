3fe40d853bdbc37aec6273ce40949d5dc85f0c55
rkri <rkri@itu.dk>
2021-09-03 14:48:24 +0200

    Fixed the problems causing it to not build

--- Assignment.Tests/UnitTest1.cs
+++ Assignment.Tests/UnitTest1.cs
@@ -2,23 +2,20 @@ using System;
 using System.IO;
 using Xunit;
 
-namespace Assignment0.Tests
+namespace Assignment.Tests
 {
     public class ProgramTests
     {
         [Fact]
         public void Main_prints_Hello_World()
         {
-            // Arrange
             var writer = new StringWriter();
             Console.SetOut(writer);
 
-            // Act
             Program.Main(new string[0]);
             var output = writer.GetStringBuilder().ToString().Trim();
 
-            // Assert
-            Assert.Equal("Hello, World!", output);
+            Assert.Equal("Hello World!", output);
         }
     }
 }
